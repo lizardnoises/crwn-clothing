050a330a17c6ea026f5207349b415518ab1661a3
Sean Moore <sean.m.moore@gmail.com>
2021-06-09 16:01:56 -0700

    Add homepage files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Homepage from './homepage';
+
+function App() {
+  return (
+    <div className='app'>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
