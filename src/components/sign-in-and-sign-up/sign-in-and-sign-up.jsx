466ea699e4d4d21b08406065ad5b543fac51d31f
Sean Moore <sean.m.moore@gmail.com>
2021-07-25 17:51:34 -0700

    Add sign-up component.

--- src/components/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import SignIn from '../sign-in/sign-in';
+import SignUp from '../sign-up/sign-up';
+import './sign-in-and-sign-up.scss';
 
 const SignInAndSignUp = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -12,6 +12,33 @@ const config = {
   measurementId: 'G-VCB578FREF',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // async API call to query database
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      // async API call to add document to database
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
